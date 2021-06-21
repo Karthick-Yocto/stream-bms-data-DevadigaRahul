dfdb6e589b0a1889971b50bcee092471010a6b33
Karthick-Yocto <68549026+Karthick-Yocto@users.noreply.github.com>
2021-06-21 17:17:14 +0530

    Update BMS_operation_manager.h

--- Receiver/BMS_operation_manager.h
+++ Receiver/BMS_operation_manager.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <functional>
+#include "Structure.h"
 
+using namespace std;
+
+class BMS_operation_manager
+{
+	vector<Parameters> _parameter_list;
+	Parameter_statistics stat;
+
+public:
+	void calculate_min_max();
+	void calculate_moving_average();
+	void print_data();
+	void perform_operation(vector<Parameters> parameter_list);
+	Parameter_statistics get_statitics();
+};
